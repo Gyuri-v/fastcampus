b8ef0ad9965eee79a147a09483f6897daf442d66
Gyuri-v <fnffkdll@naver.com>
2023-07-24 21:31:01 +0900

    cu - 4_4 그래프와 그라데이션

--- 04-04-shader-exercise-2-started/src/shaders/fragment.glsl
+++ 04-04-shader-exercise-2-started/src/shaders/fragment.glsl
@@ -1,6 +1,50 @@
 precision mediump float;
 
+varying vec2 vUv;
+
 void main()
 {
-    gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
+    // 1. 그라데이션
+    // float x = vUv.x;
+    // float y = vUv.y;
+
+    // float col = x;
+
+    // 2. 대각선 만들기
+    // float x = vUv.x;
+    // float y = vUv.y;
+
+    // vec3 col = vec3(x);
+    // vec3 green = vec3(0.0, 1.0, 0.0);
+
+    // if ( y == x ) {
+    // if ( y <= x + 0.1 ) {
+    // if ( y + 0.1 >= x ) {
+    // if ( y <= x + 0.01 && y + 0.01 >= x ) {
+    //     col = green;
+    // }
+
+    // 3. 곡선 만들기
+    // float x = vUv.x;
+    // float y = vUv.y;
+
+    // vec3 col = vec3(x);
+    // vec3 green = vec3(0.0, 1.0, 0.0);
+
+    // if ( x * x <= y && x * x >= y - 0.005 ) {
+    //     col = green;
+    // }
+
+    // 4. 곡선 그라데이션 적용
+    float x = vUv.x * 2.0;
+    float y = vUv.y;
+
+    vec3 col = vec3(x * x );
+    vec3 green = vec3(0.0, 1.0, 0.0);
+
+    if ( x * x <= y && x * x >= y - 0.005 ) {
+        col = green;
+    }
+
+    gl_FragColor = vec4(col, 1.0);
 }
